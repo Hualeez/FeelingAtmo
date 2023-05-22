879c800e98c8b956130bad3f6b100524b79f5798
Hualeez <1419680567@qq.com>
2023-05-22 12:03:13 +0800

    Initial Commit

---
+++ FeelingAtmo/FeelingAtmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelingAtmoApp.swift
+//  FeelingAtmo
+//
+//  Created by hualingz on 2023/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FeelingAtmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
